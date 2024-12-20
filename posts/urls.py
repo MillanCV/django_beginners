00b1d598c46f48fcd9859ef61284c65b3a96205a
MillanCV <m.castrovilarino@gmail.com>
2024-12-20 13:49:33 +0100

    completed chapter 6

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from posts.views import HomePageView
+from posts.views import HomePageView, PostView
 
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
+    path("post/<int:pk>/", PostView.as_view(), name="post_detail"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,12 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from posts.models import Post
 
 
 class HomePageView(ListView):
     model = Post
     template_name = "home.html"
+
+
+class PostView(DetailView):
+    model = Post
+    template_name = "post.html"
